65a6bc1fa40977ea29bf5ae35bd06eb80c30cc5f
tectoro <"mailmrsanjay64@gmail.com">
2022-05-11 14:55:33 +0530

    demo project

---
+++ springbootdemoproject-5/src/main/java/com/example/demo/Springbootdemoproject5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootdemoproject5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootdemoproject5Application.class, args);
+	}
+
+}
